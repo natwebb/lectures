1296896ec9ef5414de2bc1b41a3799d5706175b7
Nat Webb <natrwebb@gmail.com>
2014-02-26 13:09:50 -0600

    working on photo album

---
+++ 2014-02-26-photo-album/app/models/album.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = Album;
+
+var fs = require('fs');
+var path = require('path');
+//var albums = global.nss.db.collection('albums');
+//var Mongo = require('mongodb');
+//var _ = require('lodash');
+
+function Album(album){
+  this.title = album.title;
+  this.taken = new Date(album.taken);
+}
+
+Album.prototype.addCover = function(oldpath){
+  var dirname = this.title.replace(/\s/g, '').toLowerCase();
+  var newpath = __dirname + '/../static/img/' + dirname;
+  fs.mkdirSync(newpath);
+
+  var extension = path.extname(oldpath);
+  newpath += '/cover' + extension;
+  fs.renameSync(oldpath, newpath);
+
+  this.cover = path.normalize(newpath);
+};
+
---
+++ 2014-02-26-photo-album/test/unit/album_spec.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.env.DBNAME = 'albumtest';
+var expect = require('chai').expect;
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+var Album;
+
+describe('Album', function(){
+
+  before(function(done){
+    var initMongo = require('../../app/lib/init-mongo');
+    initMongo.db(function(){
+      Album = require('../../app/models/album');
+      done();
+    });
+  });
+
+  beforeEach(function(){
+    var path = __dirname + '/../../app/static/img';
+    rimraf.sync(path);
+    fs.mkdirSync(path);
+    var origfile = __dirname + '/../fixtures/euro.jpg';
+    var copyfile = __dirname + '/../fixtures/euro-copy.jpg';
+    fs.createReadStream(origfile).pipe(fs.createWriteStream(copyfile));
+  });
+
+  describe('new', function(){
+    it('should create a new album object', function(){
+      var o = {};
+      o.title = 'Euro Vacation';
+      o.taken = '2010-03-25';
+      var a1 = new Album(o);
+      expect(a1).to.be.instanceof(Album);
+      expect(a1.title).to.equal('Euro Vacation');
+      expect(a1.taken).to.be.instanceof(Date);
+    });
+  });
+
+  describe('#addCover', function(){
+    it('should add a cover to the new album', function(){
+      var o = {};
+      o.title = 'Euro Vacation';
+      o.taken = '2010-03-25';
+      var a1 = new Album(o);
+      var oldname = __dirname + '/../fixtures/euro-copy.jpg';
+      a1.addCover(oldname);
+      expect(a1.cover).to.equal(path.normalize(__dirname + '/../../app/static/img/eurovacation/cover.jpg'));
+    });
+  });
+});
